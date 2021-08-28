b36b1d389a4a38ebc8a67c99df7300bb94312259
Rickson <rickson.simoes@hotmail.com>
2021-08-27 21:04:54 -0300

    :pencil: Readme/ :hammer: CreateRental, repositories, usecases, entities, container

--- readme.md
+++ readme.md
@@ -86,3 +86,4 @@ Acessar: http://localhost:3333/api-docs/
 - O aluguel deve ter duração mínima de `24` horas.
 - Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário.
 - Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
+- O usuário deve estar logado na aplicação.
--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,22 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
+@Entity("rentals")
 class Rental {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
+  @CreateDateColumn()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
+  @Column()
   expected_return_date: Date;
 
+  @Column()
   total: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
